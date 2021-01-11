729b5067cf9a613fb1f41cf679845e947eb4dd3d
kristaponis <a.kristaponis@icloud.com>
2021-01-11 16:50:14 +0200

    Added server, Book model and handler to handle Book

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"go-rest2/handlers"
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/books", handlers.BooksHandler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("[ERROR] error starting server.", err)
+	}
 }
---
+++ handlers/handlers.go
@@ -0,0 +1,7 @@
+package handlers
+
+import "net/http"
+
+func BooksHandler(w http.ResponseWriter, r *http.Request) {
+
+}
