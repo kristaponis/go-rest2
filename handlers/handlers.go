9c93d9bb64902858c7ba909d8b20a8a7ac5f88e5
kristaponis <a.kristaponis@icloud.com>
2021-01-12 06:56:04 +0200

    Added sync.Mutex lock, content type check and json decoder

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,12 +2,16 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"go-rest2/models"
 	"log"
 	"net/http"
+	"sync"
+	"time"
 )
 
 type BooksHandler struct {
+	sync.Mutex
 	store map[string]models.Book
 }
 
@@ -28,20 +32,38 @@ func (b *BooksHandler) ServeBooks(w http.ResponseWriter, r *http.Request) {
 
 func (b *BooksHandler) get(w http.ResponseWriter, r *http.Request) {
 	books := make([]models.Book, len(b.store))
+	b.Lock()
 	i := 0
 	for _, book := range b.store {
 		books[i] = book
 		i++
 	}
+	b.Unlock()
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(books); err != nil {
 		http.Error(w, "[ERROR] error encoding json", http.StatusInternalServerError)
-		log.Fatal(err)
+		log.Println(err)
 	}
 }
 
 func (b *BooksHandler) post(w http.ResponseWriter, r *http.Request) {
+	// var body map[string]interface{}
+	var book models.Book
+	if err := json.NewDecoder(r.Body).Decode(&book); err != nil {
+		http.Error(w, "[ERROR] error decoding json", http.StatusBadRequest)
+		log.Println(err)
+		return
+	}
+	hdr := r.Header.Get("Content-Type")
+	if hdr != "application/json" {
+		http.Error(w, "[ERROR] bad Content-Type", http.StatusUnsupportedMediaType)
+		return
+	}
+	book.ID = fmt.Sprintf("%d", time.Now().UnixNano())
+	b.Lock()
+	b.store[book.ID] = book
+	defer b.Unlock()
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("POST success!"))
@@ -49,25 +71,6 @@ func (b *BooksHandler) post(w http.ResponseWriter, r *http.Request) {
 
 func NewBooksHandler() *BooksHandler {
 	return &BooksHandler{
-		store: map[string]models.Book{
-			"id1": {
-				ID:     "1",
-				Title:  "Go Programming",
-				Author: "John Johnson",
-				Year:   2020,
-			},
-			"id2": {
-				ID:     "2",
-				Title:  "Go REST",
-				Author: "Peter Peterson",
-				Year:   2018,
-			},
-			"id3": {
-				ID:     "3",
-				Title:  "Golang 'n stuff",
-				Author: "Jacob Jacobson",
-				Year:   2015,
-			},
-		},
+		store: map[string]models.Book{},
 	}
 }
